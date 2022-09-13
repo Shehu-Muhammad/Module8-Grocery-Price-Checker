051bc8748c90f8615fda0c53b87d229fad8f3aa6
Student <student@StudentiMac-33.local>
2022-09-13 11:37:46 -0400

    [UPDATE] modified sql code to create the database. added grains to the product details table and fixed categoriesId

--- Module8-Grocery-Price-Checker/SQL/module_eight_grocery_price_check.sql
+++ Module8-Grocery-Price-Checker/SQL/module_eight_grocery_price_check.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Sep 12, 2022 at 08:14 PM
+-- Generation Time: Sep 13, 2022 at 03:33 PM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -64,18 +64,18 @@ CREATE TABLE `product_details` (
 --
 
 INSERT INTO `product_details` (`id`, `productName`, `storeId`, `categoriesId`, `price`, `description`) VALUES
-(1, 'Bananas', 1, 1, 1.44, 'Most popular fruit in the world, has a yellow peel with a deliciously sweet fruit on the inside'),
-(3, 'Strawberries', 1, 1, 3.12, 'Small bright red fruit berries that are juicy and have a delicious sweet taste'),
-(4, 'Grapes', 1, 1, 4.01, 'Are firm and are juicy and sweet'),
-(5, 'Apples', 1, 1, 4.97, 'They have a sweet and aromatic flavor that makes it a great addition to any meal or even just by itself'),
-(6, 'Watermelon', 1, 1, 5.98, 'A juicy and sweet taste. Very hydrating'),
-(7, 'Oranges', 1, 1, 3.98, 'Naturally tasty oranges that are refreshingly flavorful'),
-(8, 'Bananas', 2, 1, 1.19, 'Most popular fruit in the world, has a yellow peel with a deliciously sweet fruit on the inside'),
-(9, 'Strawberries', 2, 1, 2.99, 'Small bright red fruit berries that are juicy and have a delicious sweet taste'),
-(10, 'Grapes', 2, 1, 4.39, 'Are firm and are juicy and sweet'),
-(11, 'Apples', 2, 1, 4.99, 'They have a sweet and aromatic flavor that makes it a great addition to any meal or even just by itself'),
-(12, 'Watermelon', 2, 1, 6.29, 'A juicy and sweet taste. Very hydrating'),
-(13, 'Oranges', 2, 1, 3.99, 'Naturally tasty oranges that are refreshingly flavorful'),
+(1, 'Bananas', 1, 2, 1.44, 'Most popular fruit in the world, has a yellow peel with a deliciously sweet fruit on the inside'),
+(3, 'Strawberries', 1, 2, 3.12, 'Small bright red fruit berries that are juicy and have a delicious sweet taste'),
+(4, 'Grapes', 1, 2, 4.01, 'Are firm and are juicy and sweet'),
+(5, 'Apples', 1, 2, 4.97, 'They have a sweet and aromatic flavor that makes it a great addition to any meal or even just by itself'),
+(6, 'Watermelon', 1, 2, 5.98, 'A juicy and sweet taste. Very hydrating'),
+(7, 'Oranges', 1, 2, 3.98, 'Naturally tasty oranges that are refreshingly flavorful'),
+(8, 'Bananas', 2, 2, 1.19, 'Most popular fruit in the world, has a yellow peel with a deliciously sweet fruit on the inside'),
+(9, 'Strawberries', 2, 2, 2.99, 'Small bright red fruit berries that are juicy and have a delicious sweet taste'),
+(10, 'Grapes', 2, 2, 4.39, 'Are firm and are juicy and sweet'),
+(11, 'Apples', 2, 2, 4.99, 'They have a sweet and aromatic flavor that makes it a great addition to any meal or even just by itself'),
+(12, 'Watermelon', 2, 2, 6.29, 'A juicy and sweet taste. Very hydrating'),
+(13, 'Oranges', 2, 2, 3.99, 'Naturally tasty oranges that are refreshingly flavorful'),
 (14, 'Potatoes', 1, 1, 4.37, 'When well cooked, a potato has a fluffy interior and a crisp skin'),
 (15, 'Tomatoes', 1, 1, 2.98, 'A bright, red, shiny skin delicious taste'),
 (16, 'Onions', 1, 1, 3.24, 'A round vegetable with a light brown skin. It has many white layers on its inside which have a strong, sharp smell and taste'),
@@ -88,42 +88,54 @@ INSERT INTO `product_details` (`id`, `productName`, `storeId`, `categoriesId`, `
 (23, 'Carrots', 2, 1, 2.89, 'Eat alone or enjoy with vegetable dips. Add to soups, salads, stews, and more for a healthy and tasty addition'),
 (24, 'Broccoli', 2, 1, 1.99, 'Delicious flowering heads of broccoli that make a healthy addition to any meal'),
 (25, 'Cucumbers', 2, 1, 1.59, 'Provides a delicate flavor that makes it popular in many dishes, and salads'),
-(26, 'Beef', 1, 1, 4.86, 'Clean high-quality tasty flavorful meat'),
-(27, 'Steak', 1, 1, 12.43, 'Grass fed, tender, flavorful, juicy, cook it how you prefer'),
-(28, 'Turkey', 1, 1, 7.47, 'Turkey meat is moist and tender with excellent flavor'),
-(29, 'Salmon', 1, 1, 14.36, 'A filet loaded with heart healthy omega 3 fatty acids'),
-(30, 'Tuna', 1, 1, 5.98, 'A healthful food rich in protein and contains many vitamins and minerals such as B-Complex vitamins, Vitamins A and D as well as iron, selenium and phosphorus. Tuna also contains healthy omega 3 essential fatty acids DHA and EPA.'),
-(31, 'Flounder', 1, 1, 13.96, 'Versatile, easy-to-prepare fish filet with a mildly sweet flavor and delicate, flaky texture. This firm-fleshed white fish can be baked, sautéed, stuffed, and poached.'),
-(32, 'Beef', 2, 1, 5.99, 'Clean high-quality tasty flavorful meat'),
-(33, 'Steak', 2, 1, 16.31, 'Grass fed, tender, flavorful, juicy, cook it how you prefer'),
-(34, 'Turkey', 2, 1, 6.89, 'Turkey meat is moist and tender with excellent flavor'),
-(35, 'Salmon', 2, 1, 19.99, 'A filet loaded with heart healthy omega 3 fatty acids'),
-(36, 'Tuna', 2, 1, 7.69, 'A healthful food rich in protein and contains many vitamins and minerals such as B-Complex vitamins, Vitamins A and D as well as iron, selenium and phosphorus. Tuna also contains healthy omega 3 essential fatty acids DHA and EPA.'),
-(37, 'Flounder', 2, 1, 9.99, 'Versatile, easy-to-prepare fish filet with a mildly sweet flavor and delicate, flaky texture. This firm-fleshed white fish can be baked, sautéed, stuffed, and poached.'),
-(38, 'Milk', 1, 1, 3.02, 'Provides many nutrients, including vitamin A, vitamin D, and protein'),
-(39, 'Yogurt', 1, 1, 4.36, 'Creamy flavorful good source of calcium'),
-(40, 'Cheese', 1, 1, 4.98, 'Yellow, delicious. great for sandwiches good source of calcium'),
-(41, 'Lactose-free milk', 1, 1, 6.24, 'Lactose Free milk easy to digest with the same rich taste of regular milk'),
-(42, 'Silk Milk', 1, 1, 2.98, 'Is totally free of dairy, soy, lactose, gluten, casein, egg, and MSG. And it\'s nutritious, too'),
-(43, 'Ice Cream', 1, 1, 4.97, 'A rich, sweet, creamy frozen food made from variously flavored cream and milk'),
-(44, 'Milk', 2, 1, 4.59, 'Provides many nutrients, including vitamin A, vitamin D, and protein'),
-(45, 'Yogurt', 2, 1, 4.59, 'Creamy flavorful good source of calcium'),
-(46, 'Cheese', 2, 1, 5.29, 'Yellow, delicious. great for sandwiches good source of calcium'),
-(47, 'Lactose-free milk', 2, 1, 6.69, 'Lactose Free milk easy to digest with the same rich taste of regular milk'),
-(48, 'Silk Milk', 2, 1, 2.99, 'Is totally free of dairy, soy, lactose, gluten, casein, egg, and MSG. And it\'s nutritious too'),
-(49, 'Ice Cream', 2, 1, 4.79, 'A rich, sweet, creamy frozen food made from variously flavored cream and milk'),
-(50, 'Popcorn', 1, 1, 5.16, 'Consists of grains of maize or corn that have been heated until they have burst and become large and light'),
-(51, 'Chips', 1, 1, 9.88, 'A small, thin, crisp, piece of food usually salty packed with a lot of flavor'),
-(52, 'Gushers', 1, 1, 5.16, 'It has a juicy center that bursts open with delicious fruity flavored liquid'),
-(53, 'Fruit Roll-ups', 1, 1, 5.16, 'A tasty snack flat, pectin-based, fruit-flavored rolled into a tube'),
-(54, 'Pretzels', 1, 1, 3.68, 'Perfect on their own or with cheese or hummus for a tasty snack - so many ways to enjoy'),
-(55, 'Saltines', 1, 1, 3.28, 'A thin crisp cracker usually sprinkled with salt'),
-(56, 'Popcorn', 2, 1, 6.59, 'Consists of grains of maize or corn that have been heated until they have burst and become large and light'),
-(57, 'Chips', 2, 1, 11.99, 'A small, thin, crisp, piece of food usually salty packed with a lot of flavor'),
-(58, 'Gushers', 2, 1, 5.39, 'It has a juicy center that bursts open with delicious fruity flavored liquid'),
-(59, 'Fruit Roll-ups', 2, 1, 5.49, 'A tasty snack flat, pectin-based, fruit-flavored rolled into a tube'),
-(60, 'Pretzels', 2, 1, 3.69, 'Perfect on their own or with cheese or hummus for a tasty snack - so many ways to enjoy'),
-(61, 'Saltines', 2, 1, 3.29, 'A thin crisp cracker usually sprinkled with salt.');
+(26, 'Beef', 1, 3, 4.86, 'Clean high-quality tasty flavorful meat'),
+(27, 'Steak', 1, 3, 12.43, 'Grass fed, tender, flavorful, juicy, cook it how you prefer'),
+(28, 'Turkey', 1, 3, 7.47, 'Turkey meat is moist and tender with excellent flavor'),
+(29, 'Salmon', 1, 3, 14.36, 'A filet loaded with heart healthy omega 3 fatty acids'),
+(30, 'Tuna', 1, 3, 5.98, 'A healthful food rich in protein and contains many vitamins and minerals such as B-Complex vitamins, Vitamins A and D as well as iron, selenium and phosphorus. Tuna also contains healthy omega 3 essential fatty acids DHA and EPA.'),
+(31, 'Flounder', 1, 3, 13.96, 'Versatile, easy-to-prepare fish filet with a mildly sweet flavor and delicate, flaky texture. This firm-fleshed white fish can be baked, sautéed, stuffed, and poached.'),
+(32, 'Beef', 2, 3, 5.99, 'Clean high-quality tasty flavorful meat'),
+(33, 'Steak', 2, 3, 16.31, 'Grass fed, tender, flavorful, juicy, cook it how you prefer'),
+(34, 'Turkey', 2, 3, 6.89, 'Turkey meat is moist and tender with excellent flavor'),
+(35, 'Salmon', 2, 3, 19.99, 'A filet loaded with heart healthy omega 3 fatty acids'),
+(36, 'Tuna', 2, 3, 7.69, 'A healthful food rich in protein and contains many vitamins and minerals such as B-Complex vitamins, Vitamins A and D as well as iron, selenium and phosphorus. Tuna also contains healthy omega 3 essential fatty acids DHA and EPA.'),
+(37, 'Flounder', 2, 3, 9.99, 'Versatile, easy-to-prepare fish filet with a mildly sweet flavor and delicate, flaky texture. This firm-fleshed white fish can be baked, sautéed, stuffed, and poached.'),
+(38, 'Milk', 1, 4, 3.02, 'Provides many nutrients, including vitamin A, vitamin D, and protein'),
+(39, 'Yogurt', 1, 4, 4.36, 'Creamy flavorful good source of calcium'),
+(40, 'Cheese', 1, 4, 4.98, 'Yellow, delicious. great for sandwiches good source of calcium'),
+(41, 'Lactose-free milk', 1, 4, 6.24, 'Lactose Free milk easy to digest with the same rich taste of regular milk'),
+(42, 'Silk Milk', 1, 4, 2.98, 'Is totally free of dairy, soy, lactose, gluten, casein, egg, and MSG. And it\'s nutritious, too'),
+(43, 'Ice Cream', 1, 4, 4.97, 'A rich, sweet, creamy frozen food made from variously flavored cream and milk'),
+(44, 'Milk', 2, 4, 4.59, 'Provides many nutrients, including vitamin A, vitamin D, and protein'),
+(45, 'Yogurt', 2, 4, 4.59, 'Creamy flavorful good source of calcium'),
+(46, 'Cheese', 2, 4, 5.29, 'Yellow, delicious. great for sandwiches good source of calcium'),
+(47, 'Lactose-free milk', 2, 4, 6.69, 'Lactose Free milk easy to digest with the same rich taste of regular milk'),
+(48, 'Silk Milk', 2, 4, 2.99, 'Is totally free of dairy, soy, lactose, gluten, casein, egg, and MSG. And it\'s nutritious too'),
+(49, 'Ice Cream', 2, 4, 4.79, 'A rich, sweet, creamy frozen food made from variously flavored cream and milk'),
+(50, 'Popcorn', 1, 6, 5.16, 'Consists of grains of maize or corn that have been heated until they have burst and become large and light'),
+(51, 'Chips', 1, 6, 9.88, 'A small, thin, crisp, piece of food usually salty packed with a lot of flavor'),
+(52, 'Gushers', 1, 6, 5.16, 'It has a juicy center that bursts open with delicious fruity flavored liquid'),
+(53, 'Fruit Roll-ups', 1, 6, 5.16, 'A tasty snack flat, pectin-based, fruit-flavored rolled into a tube'),
+(54, 'Pretzels', 1, 6, 3.68, 'Perfect on their own or with cheese or hummus for a tasty snack - so many ways to enjoy'),
+(55, 'Saltines', 1, 6, 3.28, 'A thin crisp cracker usually sprinkled with salt'),
+(56, 'Popcorn', 2, 6, 6.59, 'Consists of grains of maize or corn that have been heated until they have burst and become large and light'),
+(57, 'Chips', 2, 6, 11.99, 'A small, thin, crisp, piece of food usually salty packed with a lot of flavor'),
+(58, 'Gushers', 2, 6, 5.39, 'It has a juicy center that bursts open with delicious fruity flavored liquid'),
+(59, 'Fruit Roll-ups', 2, 6, 5.49, 'A tasty snack flat, pectin-based, fruit-flavored rolled into a tube'),
+(60, 'Pretzels', 2, 6, 3.69, 'Perfect on their own or with cheese or hummus for a tasty snack - so many ways to enjoy'),
+(61, 'Saltines', 2, 6, 3.29, 'A thin crisp cracker usually sprinkled with salt.'),
+(62, 'Bread', 1, 5, 3.84, 'Food made of flour, water, and yeast or another leavening agent, mixed together and baked'),
+(63, 'Pasta', 1, 5, 1.84, 'A type of food made from a mixture of flour, eggs, and water that is formed into different shapes and then boiled.'),
+(64, 'Cereal', 1, 5, 3, 'A breakfast food made from roasted grain, typically eaten with milk.'),
+(65, 'Grits', 1, 5, 2.58, 'A porridge made from boiled cornmeal'),
+(66, 'Rice', 1, 5, 1.54, 'Best cereal crop in terms of food energy per production area and is consumed in various forms, including plain rice, noodles, puffed rice, breakfast cereals, cakes, fermented sweet rice, snack foods, beer, wine and vinegar.'),
+(67, 'Flour', 1, 5, 4.48, 'Flour made from a blend of hard or soft wheats suitable for all cookery except the finest cakes.'),
+(68, 'Bread', 2, 5, 4.29, 'Food made of flour, water, and yeast or another leavening agent, mixed together and baked'),
+(69, 'Pasta', 2, 5, 1.89, 'A type of food made from a mixture of flour, eggs, and water that is formed into different shapes and then boiled.'),
+(70, 'Cereal', 2, 5, 3.79, 'A breakfast food made from roasted grain, typically eaten with milk.'),
+(71, 'Grits', 2, 5, 2.79, 'A porridge made from boiled cornmeal'),
+(72, 'Rice', 2, 5, 1.79, 'Best cereal crop in terms of food energy per production area and is consumed in various forms, including plain rice, noodles, puffed rice, breakfast cereals, cakes, fermented sweet rice, snack foods, beer, wine and vinegar.'),
+(73, 'Flour', 2, 5, 4.79, 'Flour made from a blend of hard or soft wheats suitable for all cookery except the finest cakes.');
 
 -- --------------------------------------------------------
 
@@ -232,7 +244,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `product_details`
 --
 ALTER TABLE `product_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- AUTO_INCREMENT for table `shoppinglist`
